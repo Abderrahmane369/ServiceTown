01b7564aa62360ae27cf4468c37dd385676001f5
hachimB <boubacarhachim@gmail.com>
2024-02-26 23:45:20 +0100

    adding role

--- Backend/app/models/user.rb
+++ Backend/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   belongs_to :city, type: :binary
   has_uuid :id
+  validates :role, inclusion: { in: %w(customer seller),
+    message: "%{value} is not a valid role" }, case_sensitive: false
 end
