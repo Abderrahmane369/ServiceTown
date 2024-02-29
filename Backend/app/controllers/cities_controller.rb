5f610fca675f5745e953564c2eda38d42ac882f2
hachimB <boubacarhachim@gmail.com>
2024-02-29 10:50:50 +0100

    ok

--- Backend/app/controllers/cities_controller.rb
+++ Backend/app/controllers/cities_controller.rb
@@ -45,11 +45,14 @@ class CitiesController < ApplicationController
     @city.destroy!
   end
 
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_city
-      @city = City.find(params[:id])
+     # @city = City.find(params[:id])
+       @city = City.find_by(uuid: params[:uuid])
     end
+
 
     # Only allow a list of trusted parameters through.
     def city_params
